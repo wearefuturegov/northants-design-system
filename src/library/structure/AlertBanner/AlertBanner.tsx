a7ff58a3d07a53a82b592a8168b1761dc6699da2
Anthony Taylor <antaylor@northamptonshire.gov.uk>
2021-08-05 08:24:49 +0100

    removing alert banner errors

--- src/library/structure/AlertBanner/AlertBanner.tsx
+++ src/library/structure/AlertBanner/AlertBanner.tsx
@@ -8,41 +8,41 @@ import CloseIcon from '../../components/icons/CloseIcon/CloseIcon';
 import MaxWidthContainer from "../MaxWidthContainer/MaxWidthContainer";
 
 const AlertBanner: React.FC<AlertBannerProps> = ({ uid, title, alertType = "alert", children }) => {
-    const themeContext = useContext(ThemeContext);
-    const [hideAlert, setHideAlert] = useLocalStorage(("alert_"+uid), true);
-
-    const hideClickHandler = () => {
-        setHideAlert(false)
-    }
-
-    return(
-        hideAlert &&
-            <Styles.Container alertType={alertType}>
-                <MaxWidthContainer noBackground>
-                    <Styles.InnerContainer>
-                        <Styles.BannerContentContainer>
-                            <Styles.BannerTitle>{title}</Styles.BannerTitle>
-                            <Styles.BannerContent>{children}</Styles.BannerContent>
-                        </Styles.BannerContentContainer>
-
-                        <Styles.HideLink title="Click to hide this alert banner" onClick={hideClickHandler} alertType={alertType}>
-                            <CloseIcon colourFill={alertType === "warning" ? themeContext.theme_vars.colours.black : themeContext.theme_vars.colours.white} />
-                            <br />
-                            Hide
-                        </Styles.HideLink>
-                    </Styles.InnerContainer>
-                </MaxWidthContainer>
-            </Styles.Container>
+  const themeContext = useContext(ThemeContext);
+  const [hideAlert, setHideAlert] = useLocalStorage(("alert_"+uid), true);
+  
+  const hideClickHandler = () => {
+    setHideAlert(false)
+  }
+  
+  return(
+    hideAlert &&
+    <Styles.Container alertType={alertType}>
+    <MaxWidthContainer noBackground>
+    <Styles.InnerContainer>
+    <Styles.BannerContentContainer>
+    <Styles.BannerTitle>{title}</Styles.BannerTitle>
+    <Styles.BannerContent>{children}</Styles.BannerContent>
+    </Styles.BannerContentContainer>
+    
+    <Styles.HideLink title="Click to hide this alert banner" onClick={hideClickHandler} alertType={alertType}>
+    <CloseIcon colourFill={alertType === "warning" ? themeContext.theme_vars.colours.black : themeContext.theme_vars.colours.white} />
+    <br />
+    Hide
+    </Styles.HideLink>
+    </Styles.InnerContainer>
+    </MaxWidthContainer>
+    </Styles.Container>
     );
-}
-
-function useLocalStorage(key, initialValue) {
+  }
+  
+  function useLocalStorage(key, initialValue) {
     // State to store our value
     // Pass initial state function to useState so logic is only executed once
     const [storedValue, setStoredValue] = useState(() => {
       try {
         // Get from local storage by key
-        const item = window.localStorage.getItem(key);
+        const item = (window !== 'undefined') ? window.localStorage.getItem(key) : initialValue;
         // Parse stored json or if none return initialValue
         return item ? JSON.parse(item) : initialValue;
       } catch (error) {
@@ -51,25 +51,27 @@ function useLocalStorage(key, initialValue) {
         return initialValue;
       }
     });
-  
+    
     // Return a wrapped version of useState's setter function that ...
     // ... persists the new value to localStorage.
     const setValue = value => {
       try {
         // Allow value to be a function so we have same API as useState
         const valueToStore =
-          value instanceof Function ? value(storedValue) : value;
+        value instanceof Function ? value(storedValue) : value;
         // Save state
         setStoredValue(valueToStore);
         // Save to local storage
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        if (window !== 'undefined'){
+          window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        }
       } catch (error) {
         // A more advanced implementation would handle the error case
         console.log(error);
       }
     };
-  
+    
     return [storedValue, setValue];
   }
-
-export default AlertBanner;
+  
+  export default AlertBanner;
